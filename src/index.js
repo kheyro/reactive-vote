f2ee55e0b1690bfa159cc014d7c29f1eb16fa99d
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-17 13:46:49 +0200

    [ADD] configure Store and start server

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, Route } from 'react-router';
 import configureStore from '../config/store';
-import startServer from '../config/server';
-import history from '../config/history';
-
-import App from './App';
+import startServer from '../server';
 
 const store = configureStore();
-startServer();
+startServer(store);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route component={App} />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json'),
+});
+
+store.dispatch({ type: 'NEXT' });
