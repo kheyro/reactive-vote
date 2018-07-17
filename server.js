d7be229fd52aa379f890027124d078d17bddb4e6
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-17 13:45:39 +0200

    [ADD] configure server with Express and Socket.io

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'build/')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/build/index.html`));
-});
+export default function startServer(store) {
+  app.use(express.static(path.join(__dirname, 'build/')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/build/index.html`));
+  });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+
+  store.subscribe(() => io.emit('state', store.getState().toJS()));
+  io.on('connection', socket => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+    console.log('Connection', socket);
+  });
+}
