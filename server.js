0fd4cf2e3d2f58c184695d4ef8a2ff76955ac125
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-18 21:16:24 +0200

    [ADD] fix CORS issue withCredentials and socket.io server.listen

--- server.js
+++ server.js
@@ -6,13 +6,23 @@ const io = require('socket.io')(server);
 const path = require('path');
 
 export default function startServer(store) {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+
   app.use(express.static(path.join(__dirname, 'build/')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/build/index.html`));
   });
 
   const port = process.env.PORT || 5000;
-  app.listen(port, () => console.log(`Listening on port ${port}`));
+  server.listen(port, () => console.log(`Listening on port ${port}`));
 
   store.subscribe(() => io.emit('state', store.getState().toJS()));
   io.on('connection', socket => {
