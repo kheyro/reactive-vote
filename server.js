2c42fa81d131aaeefa84c77c607538c9f4892216
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-25 15:59:24 +0200

    [UP] change static folder to public/

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ export default function startServer(store) {
     next();
   });
 
-  app.use(express.static(path.join(__dirname, 'build/')));
+  app.use(express.static(path.join(__dirname, 'public/')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/build/index.html`));
+    res.sendFile(path.join(`${__dirname}/public/index.html`));
   });
 
   const port = process.env.PORT || 5000;
