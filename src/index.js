53f266702df889efe74aadb1102e29fd11656899
Denis Atkesone <denis.atkesone@gmail.com>
2018-07-25 16:47:18 +0200

    [RF] require entries.json at top of the file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import configureStore from '../config/store';
 import startServer from '../server';
 
+const entries = require('./entries.json');
+
 const store = configureStore();
 startServer(store);
 
 store.dispatch({
   type: 'SET_ENTRIES',
-  entries: require('./entries.json'),
+  entries,
 });
 
 store.dispatch({ type: 'NEXT' });
